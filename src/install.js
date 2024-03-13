f1efd60aae57009ec9bdb876032a0213d08d3b28
Milad <khaleghi.milad.1996@gmail.com>
2024-03-13 15:31:43 +0330

    fix names

--- src/install.js
+++ src/install.js
@@ -1,22 +1,22 @@
 module.exports = function (api) {
   /**
    * This supplies a default "layout" and
-   * sets up the auto-routing folder
+   * sets up the auto-routing-windows folder
    */
   api.render('templates')
 
   /**
    * Almost all users will likely want the same setup for
-   * auto-routing, so we simply setup the
+   * auto-routing-windows, so we simply setup the
    * defaults without prompting.
    */
   api.extendJsonFile('quasar.extensions.json', {
-    'auto-routing': {
+    'auto-routing-windows': {
       pagesDir: 'src/pages',
       routePrefix: '',
       layoutsDir: 'src/layouts',
       ignorePattern: "/(^|[\/\\])\../",
-      outDir: 'src/router/auto-routing',
+      outDir: 'src/router/auto-routing-windows',
       pagesImportPrefix: 'pages/',
       generatorConfig: {
         'nested': true
